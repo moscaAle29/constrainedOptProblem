a050ab481a1f55a0c82c311c6914e11c6284d739
moscaAle29 <mosca.alessandro29@gmail.com>
2023-01-12 10:21:50 +0100

    added first doc for the steepest desc_ backtrack

--- constr_steepest_desc_bcktrck.m
+++ constr_steepest_desc_bcktrck.m
@@ -1,11 +1,37 @@
 function [xk, fk, gradfk_norm, deltaxk_norm, k, xseq, btseq] = ...
     constr_steepest_desc_bcktrck(x0, f, gradf, ...
-    kmax, tolgrad, c1, rho, btmax, gamma, tolx, Pi_X)
+    kmax, tolgrad, c1, rho, btmax, gamma, tolx,findiff_enable)
+%{
+=================================================================
+input:
+- xo -> starting point for the method
+- f -> function handle for the function to optimize
+- gradf -> function handle for the exact gradient
+- kmax -> max number of iterations for the method
+- tolgrad -> smallest norm of the the gradient
+- c1 -> constant for armijo
+- rho -> coefficient for backtrack
+- btmax -> max number of iteration for the backtrack
+- gamma -> discount factor if we are out of the feasible set
+- tolx -> 
+- findiff_enable -> enable the finite difference computation of the gradient
+
+output:
+- xk -> x value at the last iteration
+- fk -> f value at the last iteration
+- gradfk_norm -> norm of the gradient at the last iteration
+- deltaxk_norm -> difference between xk and the new direction
+- k -> last iteration of the method
+- xseq -> sequence of points the method goes through
+- btseq -> sequence of points in the backtracking method
+=================================================================
+%}
+geth=@(xhat,k) norm(xhat)*10^k;
 
 xseq = zeros(length(x0), kmax);
 btseq = zeros(1, kmax);
 
-xk = Pi_X(x0);
+xk = box_projection(x0);
 fk = f(xk);
 gradfk = gradf(xk);
 
@@ -16,11 +42,13 @@ deltaxk_norm = tolx + 1;
 farmijo = @(fk, alpha, gradfk, pk) fk + c1 * alpha * gradfk' * pk;
 
 while k < kmax && gradfk_norm >= tolgrad && deltaxk_norm >= tolx
-    
-    pk = -gradf(xk);
-    
+    if findiff_enable
+        pk=-findiff_grad(f,xk,geth(xk,k),'fw');
+    else
+        pk = -gradf(xk);
+    end
     xbark = xk + gamma * pk;
-    xhatk = Pi_X(xbark);    
+    xhatk = box_projection(xbark);    
     
     alpha = 1;
     
